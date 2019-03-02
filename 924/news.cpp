e5ecb183fa1df79ae76086d81780c66cdd9d69b7
Aditya Naik <naik.aditya23@gmail.com>
2019-03-02 04:38:17 -0500

    Starting 924

---
+++ 924/news.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int main(){
+    int num_emps;
+    cin>>num_emps;
+    int *adj[num_emps];
+    
+    for(int i=0; i<num_emps; i++){
+	arr[i] = (int*)malloc(num_emps*sizeof(int));
+    }
+}
